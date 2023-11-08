c7518b0e36375a1bb42b8af3497a12c8d6c0eef0
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-11-08 14:24:03 +1100

    Adding characters
    Added character assets and started player controller and cat script

--- Assets/Scripts/CatManager.cs
+++ Assets/Scripts/CatManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CatManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _catHearts = 3;        //catHearts = 3
+    [SerializeField] private bool _isFollowing = true;  //isFollowing = true
+    [SerializeField] private GameObject _player;        //player = reference to PlayerObject
+
+    private void Update()
     {
-        
+        if (_isFollowing)
+        {
+            FollowPlayer();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Function to make the cat follow the player
+    //function followPlayer():
+    private void FollowPlayer()
     {
-        
+        //     Calculate the cat's movement towards the player
+        //     Update cat position to follow the player
+        gameObject.transform.position = _player.transform.position;
     }
+
+    //     Function to toggle following behavior on/off
+    //function toggleFollowing() :
+    public void ToggleFollowing()
+    {
+        _isFollowing = !_isFollowing;
+    }
+
+    // Function to reduce cat hearts
+    private void ReduceHearts()
+    {
+        //_catHearts -= damage;
+    }
+
+    // Function to check if the cat is in range of an enemy
+    //function isInRange(enemy) :
+    //     Implement logic to check if cat is in enemy's attack range
 }
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // set player health
-    // reference cat object
-    // reference enemy object
+    [SerializeField] private GameObject _cat;       // reference cat object
+    [SerializeField] private EnemyManager _enemy;   // reference enemy object
+
+    private int _playerHearts = 5; // set player health
+
+    [SerializeField] private Rigidbody2D _body;
+
+    private float _horizontal;
+    private float _vertical;
+    private float _moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,6 +30,9 @@ public class CharacterController : MonoBehaviour
         //if (input(A)) moveLeft()
         //if (input(S)) moveBackward()
         //if (input(D)) moveRight()
+
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
 
         // Handle interaction with the cat
         //if (input(E)):
@@ -28,6 +45,17 @@ public class CharacterController : MonoBehaviour
         // Check for player health reaching zero
         //if (playerHearts <= 0):
         //    gameOver()
+    }
+    
+    void FixedUpdate()
+    {
+        if (_horizontal != 0 && _vertical != 0) // CHecking for diagonal movement
+        {
+            // Limit movement speed diagonally, so you move at 70% speed
+            _horizontal *= _moveLimiter; 
+            _vertical *= _moveLimiter;
+        }
+        _body.velocity = new Vector2(_horizontal * runSpeed, _vertical * runSpeed);
     }
 
     //// Interact with the cat
